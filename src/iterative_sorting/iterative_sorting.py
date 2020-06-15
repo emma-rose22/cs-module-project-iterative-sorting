9cbdaae222e739ea2935661035cb9d8d2a2729e7
emma-rose22 <57078967+emma-rose22@users.noreply.github.com>
2020-06-15 19:08:34 -0400

    monday done

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -2,23 +2,50 @@
 def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
-        # Your code here
+  
+        #notes from tk
+        # assume index 0 is smallest value
+        # begin by looking for the smallest value
+        # swap it with index 0
+        # update index to be index 1 
+        # do this until the index we are looking at is the last index
 
+        #assume index 0 is smallest value
+        cur_index = i
+        smallest_index = cur_index
 
-        # TO-DO: swap
-        # Your code here
+        #look for the smallest value
+        for x in range(i + 1, len(arr)):
+            if arr[smallest_index] > arr[x]:
+                smallest_index = x
+
+        print('before switch:', arr[i], arr[smallest_index])
+
+        arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
+        cur_index += 1
+        print('I just flipped the switch:', arr[i], arr[smallest_index])
+        print('arr:', arr)
+        print()
 
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-    # Your code here
+    #repeat until there are no more swaps
+    swap = 1
 
+    while swap > 0:
+        swap = 0
+        for i in range(len(arr) -1):
+            # if the card on right is smaller than left
+            if arr[i + 1] < arr[i]:
+                #swap
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                # repeat until there are no more swaps
+                swap += 1
 
     return arr
 
@@ -29,3 +56,10 @@ def count_sort(arr, maximum=-1):
 
 
     return arr
+
+
+numbers = [2, 4, 3, 5, 8 , 7, 1]
+
+#print(selection_sort(numbers))
+
+print(bubble_sort(numbers))
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,41 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    first = 0
+    middle = len(arr) // 2
+    last = len(arr) - 1
 
-    # Your code here
+    found = False
+    # we start by searching the very first value
+    while first <= last and not found:
+        middle = (first + last) // 2
 
+        if arr[middle] == target:
+            found = True
+            return middle
+
+        else:
+        # if the target is less than the current value
+        #  move the middle to the left one, and reset
+        # the last value to be the middle minus one
+        # cutting our search feild in half
+            if arr[middle] > target:
+                last = middle - 1
+
+        #if the target is greater than the middle value
+        # we reset the first value to be one above the middle value
+        #again cutting the search in half
+            else:
+                first = middle + 1
 
     return -1  # not found
+
+
